44e33ce280ffa62a32c631909314c569d88fdb4c
mrumsey0 <89649954+mrumsey0@users.noreply.github.com>
2023-11-05 23:53:53 -0500

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-output "public_ip_address" {
-    value = azurerm_linux_virtual_machine.vm.public_ip_address
-}
-
-output "tls_private_key" {
-    value = tls_private_key.ssh_key.private_key_pem
-    sensitive = true
-}
